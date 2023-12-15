efca26d42c699e21d445879f65df52ebc165d70c
Karl Brycz <kbrycz@umich.edu>
2023-12-15 12:43:56 -0600

    Initial Commit

---
+++ Buddi/BuddiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddiApp.swift
+//  Buddi
+//
+//  Created by Karl Brycz on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BuddiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
